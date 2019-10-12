b47d68c12c0d819be3c566d834f6445b925a5b17
Aqib Javed <aqibbutt3078@gmail.com>
2019-10-12 19:23:24 +0500

    Initial Commit

---
+++ adidas-subscription-api/src/test/java/com/adidas/subscription/AdidasSubscriptionApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adidas.subscription;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdidasSubscriptionApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
